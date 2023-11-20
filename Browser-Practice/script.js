12b8ac9556bc39fc187958f1bd5683e6d6bf5bf3
MaksymZhos <148744478+MaksymZhos@users.noreply.github.com>
2023-11-20 11:46:14 -0800

    Adding Script Stuff before I head out to a meeting

---
+++ Browser-Practice/script.js
@@ -0,0 +1,41 @@
+//Functions we Might Need
+
+//roll die function
+function roll_die(number=6) {
+    return Math.floor(Math.random()*number);
+  }
+
+//Function to generate enemy traits
+//Do Later
+//The idea is that we feed it a dictionary (or many) of traits and it randomly assigns them to a random enemy
+var current_enemy = {};
+
+// Sample Data Structure for Enemy: Name, Type, Description, health stat, attack_stat, mana_stat
+var enemy_dictionary = {names: ["Alpha", "Beta", "Gamma"], types: ["water", "fire", "electric"]};
+
+function create_enemy (enemy_dictionary) {
+    let name_length = enemy_dictionary['names'].length;
+    let name_random = roll_die(name_length);
+    console.log(`Testing Name generation. Name length is ${name_length} and random name chosen is ${enemy_dictionary['names'][name_random]}`);
+    let type_length = enemy_dictionary['types'].length;
+    let type_random = roll_die(type_length);
+    console.log(`Testing Type generation. Type length is ${type_length} and random name chosen is ${enemy_dictionary['types'][type_random]}`);
+
+
+
+
+
+    
+}
+
+create_enemy(enemy_dictionary);
+
+
+
+// We need an event listener for when the page loads
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+});
+
