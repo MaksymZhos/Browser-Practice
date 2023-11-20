d6f815e9b71530acc5d65b629ae5065d6c6f5b64
MaksymZhos <148744478+MaksymZhos@users.noreply.github.com>
2023-11-20 12:06:30 -0800

    Made a rough version of the enemy generation function. I want to do a battle function later

--- Browser-Practice/script.js
+++ Browser-Practice/script.js
@@ -8,27 +8,33 @@ function roll_die(number=6) {
 //Function to generate enemy traits
 //Do Later
 //The idea is that we feed it a dictionary (or many) of traits and it randomly assigns them to a random enemy
-var current_enemy = {};
+var current_enemy = [];
 
 // Sample Data Structure for Enemy: Name, Type, Description, health stat, attack_stat, mana_stat
 var enemy_dictionary = {names: ["Alpha", "Beta", "Gamma"], types: ["water", "fire", "electric"]};
 
 function create_enemy (enemy_dictionary) {
+    //let returned_enemy = [];
     let name_length = enemy_dictionary['names'].length;
     let name_random = roll_die(name_length);
-    console.log(`Testing Name generation. Name length is ${name_length} and random name chosen is ${enemy_dictionary['names'][name_random]}`);
+    let selected_name = enemy_dictionary['names'][name_random];
+
+    // console.log(`Testing Name generation. Name length is ${name_length} and random name chosen is ${enemy_dictionary['names'][name_random]}`);
+    console.log(`Testing Name generation. Name length is ${name_length} and random name chosen is ${selected_name}`);
+
     let type_length = enemy_dictionary['types'].length;
     let type_random = roll_die(type_length);
-    console.log(`Testing Type generation. Type length is ${type_length} and random name chosen is ${enemy_dictionary['types'][type_random]}`);
+    let selected_type = enemy_dictionary['types'][type_random];
+    // console.log(`Testing Type generation. Type length is ${type_length} and random name chosen is ${enemy_dictionary['types'][type_random]}`);
+    console.log(`Testing Type generation. Type length is ${type_length} and random name chosen is ${selected_type}`);
 
+    return selected_name, selected_type;
 
-
-
-
-    
 }
 
 create_enemy(enemy_dictionary);
+
+//Do Later. Create a Function to do the Battle Stuff
 
 
 
